762d28174d3247a5f19aa0c03ed28359fd7e71a0
Oein <oein0219@gmail.com>
2022-07-25 18:52:31 +0900

    NextUI and react-toastify

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,19 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { NextUIProvider } from "@nextui-org/react";
+import { ToastContainer } from "react-toastify";
+
+import "react-toastify/dist/ReactToastify.min.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <NextUIProvider>
+      <>
+        <ToastContainer autoClose={3000} position={"bottom-center"} />
+        <Component {...pageProps} />
+      </>
+    </NextUIProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
